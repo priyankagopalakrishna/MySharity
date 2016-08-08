8a685c6b09213321d97a21b0bb58c6493c7d67d8
Rogue <santacruz>
2016-08-08 16:39:02 -0700

    Google sign in

--- app/src/main/java/com/android/sharity/mysharity/MainActivity1.java
+++ app/src/main/java/com/android/sharity/mysharity/MainActivity1.java
@@ -30,6 +30,7 @@ import com.google.android.gms.common.api.Status;
 import com.google.android.gms.plus.Plus;
 import com.google.android.gms.plus.model.people.Person;
 
+//TEst
 public class MainActivity1 extends Activity implements OnClickListener,
         ConnectionCallbacks, OnConnectionFailedListener {
 
